1bd372d9e182a089187985fa5efdedd5f8563c66
hgoose <90582417+hgoose@users.noreply.github.com>
2025-02-04 18:33:04 +0000

    Co-authored-by: matt <mattgh2@users.noreply.github.com>

--- main.cc
+++ main.cc
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <ranges>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
 
 auto main(int argc, char** argv) -> int {
-    return EXIT_SUCESS;
+
+    void* memory = operator new(sizeof(point));
+    point* ptr = static_cast<point*>(memory);
+    new (memory) point();
+
+    ptr->print();
+    vector<int> v{1,2,3,4,5};
+    
+    auto io = std::ranges::views::iota(0,5)
+
+    std::print(sizeof(io));
+
+
+    for (int i=0; i<5; ++i);
+    for (const auto& i : std::ranges::views::iota(0,5));
+
+
+
+for (const auto& item : io) cout << item << " ";    }
+ 
+ 
+   return EXIT_SUCCESS;
 }
